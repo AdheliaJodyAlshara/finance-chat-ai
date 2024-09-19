e6461fdb6f288787d0ed2a3ec231c890162cd56c
Adhelia Jody <adhelia.jody@techconnect.co.id>
2024-09-20 00:14:14 +0700

    data numeric format

--- config.py
+++ config.py
@@ -2,6 +2,8 @@ import os
 import pandas as pd
 from langchain_openai import ChatOpenAI
 
+pd.set_option('display.float_format', '{:.2f}'.format)
+
 url = os.getenv("CSV_URL")
 file_id=url.split('/')[-2]
 dwn_url='https://drive.google.com/uc?id=' + file_id
